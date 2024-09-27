22b40a0e81dba8e94f1212c47b7f05e5c08ae50b
kajal2597 <kajalg2597@gmail.com>
2024-09-27 14:19:15 +0530

    Added:Employee wage Using Switch Case

--- src/com/Bridzlab/EmployeeWage/EmployeeWageComputation.java
+++ src/com/Bridzlab/EmployeeWage/EmployeeWageComputation.java
@@ -3,32 +3,30 @@ Author:Kajal Gaikwad
 Purpose: Calculate daily wage for full-time and part-time employees based on attendance using switch-case statement.
 Date: 26/9/24
 */
-//Add Part time Employee & Wage
+//Add Part time Employee & Wage Using Switch Case
 package com.Bridzlab.EmployeeWage;
-
 public class EmployeeWageComputation {
     public static void main(String args[])
     {
-        int FULL_TIME = 2;
-        int PART_TIME = 1;
+        final int PART_TIME = 1;
+        final int FULL_TIME = 2;
         int WAGE_PER_HR = 20;
-        int workingHours = 0;
-        double empType = Math.floor(Math.random() * 10) % 3; //Math Floor Function
-        if (empType == FULL_TIME)
+        int empType = (int) (Math.random() * 10) % 3;
+        int workingHours =0;
+        switch (empType)
         {
-            System.out.println("Employee is Present Full Time");
-            workingHours = 8;
+            case FULL_TIME:
+                System.out.println("Employee is Present Full time");
+                workingHours = 8;
+                break;
+            case PART_TIME:
+                System.out.println("Employee is Present Part time");
+                workingHours = 4;
+                break;
+            default:
+                System.out.println("Employee is Absent");
         }
-        else if (empType == PART_TIME)
-        {
-            System.out.println("Employee is Present Part Time");
-            workingHours = 4;
-        }
-        else
-        {
-            System.out.println("Employee is Absent");
-        }
-        int wage = workingHours * WAGE_PER_HR; //Calculate wage
+        int wage = workingHours * WAGE_PER_HR;
         System.out.println("Employee Daily Wage is " + wage);
     }
 }
