88d2575e768d7a7249a8d78e1491da885f3a664d
kajal2597 <kajalg2597@gmail.com>
2024-09-27 14:48:23 +0530

    uc6:Updated-Calculate wages till condition of total working hours or days is reached for a month

--- src/com/Bridzlab/EmployeeWage/EmployeeWageComputation.java
+++ src/com/Bridzlab/EmployeeWage/EmployeeWageComputation.java
@@ -4,10 +4,11 @@ Purpose: Calculate daily wage for full-time and part-time employees based on att
 Date: 26/9/24
 */
 //UC1 Check employee is present or absent
-//UC2 Updated Calculate daily employee wage
-//UC3 Updated part-time employee & wage
-//UC4 Updated Using switch case statement
-//UC5 Updated Calculating wages for a month
+//UC2 Calculate daily employee wage
+//UC3 Add part-time employee & wage
+//UC4 Using switch case statement
+//UC5 Calculating wages for a month
+//UC6 Calculate wages till condition of total working hours or days is reached for a month
 package com.Bridzlab.EmployeeWage;
 public class EmployeeWageComputation {
     public static void main(String args[])
@@ -15,26 +16,31 @@ public class EmployeeWageComputation {
         final int PART_TIME = 1;
         final int FULL_TIME = 2;
         final int WAGE_PER_HR = 20;
-        final int WORKING_DAYS = 30;
+        final int MAX_WORKING_DAYS = 20;
+        final int MAX_WORKING_HRS = 100;
         int totalWage = 0;
-        for (int day = 1; day <= WORKING_DAYS; day++)
+        int workingHrs = 0;
+        System.out.printf("%5s     %5s     %5s     %5s\n", "Day", "Workinghrs", "Wage", "Total working hrs");
+        for (int day = 1, totalWorkingHrs = 0; day <= MAX_WORKING_DAYS && totalWorkingHrs < MAX_WORKING_HRS; day++, totalWorkingHrs += workingHrs)
         {
             int empType = (int) (Math.random() * 100) % 3;
-            int workingHours =0;
             switch (empType)
             {
                 case FULL_TIME:
-                    workingHours = 8;
+                    workingHrs = 8;
                     break;
                 case PART_TIME:
-                    workingHours = 4;
+                    workingHrs = 4;
                     break;
                 default:
+                    workingHrs = 0;
+                    break;
             }
-            int wage = workingHours * WAGE_PER_HR;
-            System.out.println("Day " + day + " wage is:" + wage);
+            int wage = workingHrs * WAGE_PER_HR;
             totalWage += wage;
+            System.out.printf("%5d       %5d      %5d      %5d\n", day, workingHrs, wage, totalWorkingHrs + workingHrs);
         }
         System.out.println("Total wage for a month is " + totalWage);
     }
+
 }
